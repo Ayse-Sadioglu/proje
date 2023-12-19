2af65ac9562c5ea3fe71993b654ea75d941d78ad
Ayse <aysesadioglu@protonmail.com>
2023-12-19 20:10:20 +0300

    integrity check added

--- cyber.py
+++ cyber.py
@@ -26,21 +26,54 @@ def encrypt(key, filename, output_path):
 
                 outfile.write(encryptor.encrypt(chunk))
 
+    # Calculate the hash of the ciphertext
+    hash_obj = SHA256.new()
+    with open(outputFile, 'rb') as encrypted_file:
+        while True:
+            chunk = encrypted_file.read(chunksize)
+            if not chunk:
+                break
+            hash_obj.update(chunk)
+
+    # Write the hash to a separate file
+    hash_file_path = os.path.join(output_path, "enc_" + os.path.basename(filename) + ".hash")
+    with open(hash_file_path, 'w') as hash_file:
+        hash_file.write(hash_obj.hexdigest())
+
 def decrypt(key, filename, output_path):
     chunksize = 64 * 1024
     original_filename = os.path.basename(filename)[4:]  # Remove the "(enc)" prefix
     outputFile = os.path.join(output_path, "dec_" + original_filename.strip())  # Prepend "dec_"
+    hash_file_path = os.path.join(output_path, "enc_" + original_filename.strip() + ".hash")
 
     # Check if the directory exists
     if not os.path.isdir(output_path):
         raise FileNotFoundError(f"The specified output path '{output_path}' is not a directory or does not exist.")
 
-    print(f"Output file path: {outputFile}")  
+    print(f"Output file path: {outputFile}")
 
+    # Read the stored hash from the hash file
+    with open(hash_file_path, 'r') as hash_file:
+        stored_hash = hash_file.read().strip()
+
+    # Calculate the hash of the ciphertext before decryption
+    hash_obj = SHA256.new()
+    with open(filename, 'rb') as encrypted_file:
+        while True:
+            chunk = encrypted_file.read(chunksize)
+            if not chunk:
+                break
+            hash_obj.update(chunk)
+
+    # Compare the calculated hash with the stored hash
+    integrity_hash = hash_obj.hexdigest()
+    if integrity_hash != stored_hash:
+        print("Integrity check failed. The encrypted file may be corrupted.")
+
+    # Proceed with decryption
     with open(filename, 'rb') as infile:
         filesize = int(infile.read(16))
         IV = infile.read(16)
-
         decryptor = AES.new(key, AES.MODE_CBC, IV)
 
         with open(outputFile, 'wb') as outfile:
@@ -54,7 +87,6 @@ def decrypt(key, filename, output_path):
 
             outfile.truncate(filesize)
 
-
 def getKey(password):
     hasher = SHA256.new(password.encode('utf-8'))
     return hasher.digest()
@@ -67,14 +99,14 @@ def Main():
         key = input("Key: ")
         output_path = input("Enter the path to save the encrypted file: ")
         encrypt(getKey(key), filename, output_path)
-        print('Done.')
     elif choice == 'D':
         filename = input("File to decrypt: ")
         key = input("Key: ")
         output_path = input("Enter the path to save the decrypted file: ")
         decrypt(getKey(key), filename, output_path)
-        print("Done.")
+        
     else:
         print("No option selected, closing...")
 
-Main()
+if __name__ == "__main__":
+    Main()
